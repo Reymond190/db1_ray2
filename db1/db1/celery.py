3e1fef16cfbe3e45d35bb40990681382f93b4ce0
joseph reymond <josephreymond2011@gmail.com>
2020-02-27 12:56:06 +0530

    state1

--- db1/db1/celery.py
+++ db1/db1/celery.py
@@ -1,6 +1,6 @@
 import os
 from celery import Celery
-from one.tasks import all2
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'db1.settings')
 
@@ -10,7 +10,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 
 
-from one.tasks import all2
 
 app.conf.beat_schedule = {
     "see-you-in-ten-seconds-task": {
