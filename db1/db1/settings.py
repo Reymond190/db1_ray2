c6cddbd334b02effe7f84996774259bfe877d2ed
joseph reymond <josephreymond2011@gmail.com>
2020-02-28 13:41:19 +0530

    currentspeed

--- db1/db1/settings.py
+++ db1/db1/settings.py
@@ -135,9 +135,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static-cdn", "static_root")
 
 
 # CELERY_RESULT_BACKEND = 'amqp://jimmy:jimmy123@localhost/jimmy_vhost'
