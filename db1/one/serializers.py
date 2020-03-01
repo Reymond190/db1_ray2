deda2fdd23b0ce01e59493efd7805458feacd9f9
joseph reymond <josephreymond2011@gmail.com>
2020-03-02 01:29:20 +0530

    changes

--- db1/one/serializers.py
+++ db1/one/serializers.py
@@ -15,5 +15,5 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 class Serialize2(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = api1
-        fields = "__all__"
+        fields = ['id','Total','Running','Idle','Stop','Inactive','No_of_overspeed','No_of_geofence']
 
